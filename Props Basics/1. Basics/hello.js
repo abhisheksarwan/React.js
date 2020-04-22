0eae91621c4127a7c01e43afbc7b228a01d0dfee
worstguyalive <worstguyalive@localhost.localdomain>
2020-04-23 01:43:50 +0530

    added state

--- Props Basics/1. Basics/hello.js
+++ Props Basics/1. Basics/hello.js
@@ -1,11 +1,12 @@
 class Hello extends React.Component {
     render(){
+        let style ={ color:"red"};
         console.log(this.props);
         //you can also use
         // const props = this.props and then prop.to and so on
         return (
             <div>
-            <h1>Hello! from {this.props.to} from {this.props.from} my number is {this.props.number}</h1>
+            <h1  style={style}>Hello! from {this.props.to} from {this.props.from} my number is {this.props.number}</h1>
             <img src={this.props.img} height="200" />
             </div>
             )
